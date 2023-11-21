fd43a4f9bed9e406b8fc16f104570bc311473a01
Tim <tbettison@deloitte.co.uk>
2023-11-21 21:59:34 +0000

    build fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div className="grid grid-cols-3 grid-rows-6">
         <div className="col-start-2 row-start-2 flex flex-col justify-center items-center">
-            <p className="text-2xl pb-3">Hi, I'm Tim. 👋</p>
-            <p>I'm a Software Engineer who loves to build things.</p>
+            <p className="text-2xl pb-3">Hi, I&apos;m Tim. 👋</p>
+            <p>I&apos;m a Software Engineer who loves to build things.</p>
         </div>
     </div>
   )
