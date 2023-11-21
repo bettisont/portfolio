041e0a065a52d47ca4c5760fb9c1a8153ad7ebab
Tim <tbettison@deloitte.co.uk>
2023-11-21 21:54:45 +0000

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-3 grid-rows-6 min-h-screen">
+    <div className="grid grid-cols-3 grid-rows-6">
         <div className="col-start-2 row-start-2 flex flex-col justify-center items-center">
             <p className="text-2xl pb-3">Hi, I'm Tim. 👋</p>
             <p>I'm a Software Engineer who loves to build things.</p>
